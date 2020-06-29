f475d694dc3581608780a071a7381b7ddc2483c9
rommyk <baron1xxx@gmail.com>
2020-06-29 18:27:30 +0300

    DB init connect

--- src/server.js
+++ src/server.js
@@ -2,11 +2,23 @@ import express from 'express';
 import http from 'http';
 import socketIO from 'socket.io';
 import env from './env';
+import sequelize from './data/db/connection';
 
 const app = express();
 const socketServer = http.Server(app);
 // eslint-disable-next-line no-unused-vars
 const io = socketIO(socketServer);
+
+sequelize
+  .authenticate()
+  .then(() => {
+    // eslint-disable-next-line no-console
+    console.log('Connection has been established successfully.');
+  })
+  .catch(err => {
+    // eslint-disable-next-line no-console
+    console.error('Unable to connect to the database:', err);
+  });
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));

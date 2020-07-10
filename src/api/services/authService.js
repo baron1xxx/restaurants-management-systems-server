5c6d213f98f3e9e966a2a428c1f4e2dba448b0db
rommyk <baron1xxx@gmail.com>
2020-07-10 21:35:04 +0300

    Add login service

--- src/api/services/authService.js
+++ src/api/services/authService.js
@@ -1,13 +1,14 @@
 import userRepository from '../../data/repositories/userRepository';
 import roleRepository from '../../data/repositories/roleRepository';
 import credentialRepository from '../../data/repositories/credentialRepository';
+import authTokenRepository from '../../data/repositories/authTokenRepository';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
-import { encrypt } from '../../helpers/cryptoHelper';
+import { encrypt, compare } from '../../helpers/cryptoHelper';
 import { createToken } from '../../helpers/tokenHelper';
 import { emailTransporter } from '../../helpers/emailTransporter';
 import { authErrorMessages } from '../../constants/customErrorMessage/authErrorMessage';
 import { UNAUTHORIZED } from '../../constants/responseStatusCodes';
-import { REGISTER_CONTROLLER } from '../../constants/controllerName/authControllerName';
+import { REGISTER_CONTROLLER, LOGIN_CONTROLLER } from '../../constants/controllerName/authControllerName';
 import { ACTIVATE_ACCOUNT } from '../../constants/emailSubject';
 import { ownerSecret } from '../../config/jwtConfig';
 import { activateAccountTemplate } from '../../template/emailTemplate';
@@ -36,6 +37,60 @@ export const register = async ({ email, password, authMethod, role, ...userData 
     );
 
     return user;
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, e.controller);
+  }
+};
+
+export const login = async ({ email, password, authMethod }) => {
+  try {
+    const credentialByEmail = await credentialRepository.getByEmail(email);
+    const { user } = credentialByEmail;
+    if (!credentialByEmail) {
+      throw new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.USER_NOT_FOUND,
+        LOGIN_CONTROLLER
+      );
+    }
+
+    if (credentialByEmail.authMethod !== authMethod) {
+      throw new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.SIGN_IN_WITH + credentialByEmail.authMethod,
+        LOGIN_CONTROLLER
+      );
+    }
+    if (!await compare(password, credentialByEmail.password)) {
+      throw new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.PASSWORDS_NOT_MATCH,
+        LOGIN_CONTROLLER
+      );
+    }
+    if (!user.isActivated) {
+      throw new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.USER_NOT_ACTIVATED,
+        LOGIN_CONTROLLER
+      );
+    }
+    if (user.isBlocked) {
+      throw new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.USER_BLOCKED,
+        LOGIN_CONTROLLER
+      );
+    }
+
+    const accessToken = createToken({}, ownerSecret.accessToken, '15m');
+    const refreshToken = createToken({}, ownerSecret.refreshToken, '1d');
+
+    await authTokenRepository.create({ accessToken, refreshToken, userId: user.id });
+
+    return {
+      accessToken: `Bearer ${accessToken}`,
+      refreshToken };
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, e.controller);
   }

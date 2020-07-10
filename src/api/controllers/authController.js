8a90d331cfed522a5551d491511c7ab716fc5a6f
rommyk <baron1xxx@gmail.com>
2020-07-10 21:33:47 +0300

    Add login controller

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -14,3 +14,20 @@ export const register = async (req, res, next) => {
     next({ status: e.status, message: e.message, controller: e.controller });
   }
 };
+
+export const login = async (req, res, next) => {
+  try {
+    const { accessToken, refreshToken } = await userService.login(req.body);
+    res.status(200)
+      .json({
+        error: false,
+        data: {
+          accessToken,
+          refreshToken
+        },
+        message: ''
+      });
+  } catch (e) {
+    next({ status: e.status, message: e.message, controller: e.controller });
+  }
+};

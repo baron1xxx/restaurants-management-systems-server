68587463c0b06cdb069b178ee1edf48b01ceed7b
rommyk <baron1xxx@gmail.com>
2020-07-10 21:33:02 +0300

    Add auth/login router

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -1,10 +1,12 @@
 import { Router } from 'express';
 import userValidateMiddleware from '../middlewares/userMiddlewares/userValidateMiddleware';
+import loginValidateMiddleware from '../middlewares/authMiddlewares/loginValidateMiddleware';
 import * as authController from '../controllers/authController';
 
 const router = Router();
 
 router
-  .post('/register', userValidateMiddleware, authController.register);
+  .post('/register', userValidateMiddleware, authController.register)
+  .post('/login', loginValidateMiddleware, authController.login);
 
 export default router;

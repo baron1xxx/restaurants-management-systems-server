2261785d22b126c296d025fed3c9fdd65a80dffe
rommyk <baron1xxx@gmail.com>
2020-07-15 14:28:28 +0300

    Add imageRepository

---
+++ src/data/repositories/imageRepository.js
@@ -0,0 +1,8 @@
+import BaseRepository from './baseRepository';
+import { ImageModel } from '../models';
+
+class ImageRepository extends BaseRepository {
+
+}
+
+export default new ImageRepository(ImageModel);

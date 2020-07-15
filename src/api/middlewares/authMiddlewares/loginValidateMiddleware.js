caab697fa21427bc74a2264d93bdd2fc772e670a
rommyk <baron1xxx@gmail.com>
2020-07-15 15:02:41 +0300

    Update ErrorHandler constructor (add controller text)

--- src/api/middlewares/authMiddlewares/loginValidateMiddleware.js
+++ src/api/middlewares/authMiddlewares/loginValidateMiddleware.js
@@ -1,11 +1,17 @@
 import { loginValidator } from '../../../validators/loginValidator';
+import { UNAUTHORIZED } from '../../../constants/responseStatusCodes';
 
 export default (req, res, next) => {
   try {
     const { error } = loginValidator.validate(req.body);
-    if (error) next({ status: 400, message: error.details[0].message });
+    if (error) {
+      next({
+        status: UNAUTHORIZED,
+        message: error.details[0].message,
+        controller: 'loginValidateMiddleware' });
+    }
     next();
   } catch (e) {
-    next({ status: e.status, message: e.message });
+    next({ status: e.status, message: e.message, controller: e.controller });
   }
 };
--- src/helpers/tokenHelper.js
+++ src/helpers/tokenHelper.js
@@ -1,8 +1,10 @@
 import jwt from 'jsonwebtoken';
+import { ErrorHandler } from './error/ErrorHandler';
+import { UNAUTHORIZED } from '../constants/responseStatusCodes';
 
 export const createToken = (data, secret, expiresIn) => jwt.sign(data, secret, { expiresIn });
 
 export const parseToken = (token, secret) => jwt.verify(token, secret, (err, decode) => {
-  if (err) throw new Error('Token is invalid.');
+  if (err) throw new ErrorHandler(UNAUTHORIZED, 'Token is invalid.', 'Parse token');
   return decode;
 });

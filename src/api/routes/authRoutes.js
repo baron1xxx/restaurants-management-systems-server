3c45503c40074bb1a3fca8e8c199e99f5ca8bb9d
rommyk <baron1xxx@gmail.com>
2020-07-15 13:51:31 +0300

    Change google router (comment auth google strategy)

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import userValidateMiddleware from '../middlewares/userMiddlewares/userValidateMiddleware';
 import loginValidateMiddleware from '../middlewares/authMiddlewares/loginValidateMiddleware';
+import googleLoginValidateMiddleware from '../middlewares/authMiddlewares/googleLoginValidateMiddleware';
 import * as authController from '../controllers/authController';
 
 const router = Router();
@@ -9,5 +10,13 @@ router
   .post('/register', userValidateMiddleware, authController.register)
   .post('/login', loginValidateMiddleware, authController.login)
   .post('/google', googleLoginValidateMiddleware, authController.google);
+
+// Test passport google authentication
+
+// router
+//   .get('/google', registrationGoogleMiddleware)
+//   .get('/google/callback', registrationGoogleCallbackMiddleware, (req, res) => {
+//     res.json(req.user);
+//   });
 
 export default router;

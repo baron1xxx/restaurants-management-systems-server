3ade8f9e055739e13218c0d062f059c9235147c3
rommyk <baron1xxx@gmail.com>
2020-07-15 14:32:43 +0300

    Add auth/activate/:token router

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -2,11 +2,13 @@ import { Router } from 'express';
 import userValidateMiddleware from '../middlewares/userMiddlewares/userValidateMiddleware';
 import loginValidateMiddleware from '../middlewares/authMiddlewares/loginValidateMiddleware';
 import googleLoginValidateMiddleware from '../middlewares/authMiddlewares/googleLoginValidateMiddleware';
+import checkActivateTokenMiddleware from '../middlewares/authMiddlewares/checkActivateTokenMiddleware';
 import * as authController from '../controllers/authController';
 
 const router = Router();
 
 router
+  .get('/activate/:token', checkActivateTokenMiddleware, authController.activate)
   .post('/register', userValidateMiddleware, authController.register)
   .post('/login', loginValidateMiddleware, authController.login)
   .post('/google', googleLoginValidateMiddleware, authController.google);

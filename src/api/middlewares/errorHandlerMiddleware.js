d1e475bd9cf85f460cca5729ea9934fb1a1216b3
rommyk <baron1xxx@gmail.com>
2020-07-25 14:31:24 +0300

    Add to constants AUTH_USER_SERVICE name

--- src/api/middlewares/errorHandlerMiddleware.js
+++ src/api/middlewares/errorHandlerMiddleware.js
@@ -1,6 +1,4 @@
 export default (err, req, res, next) => {
-  // eslint-disable-next-line no-console
-  console.log(err);
   const { status = 500, message = '', parent = undefined, controller = '' } = err;
   if (parent) res.status(status).json({ error: true, status, message: parent.sqlMessage, controller });
   else res.status(status).json({ error: true, status, message, controller });
--- src/constants/servicesName/authServicesName.js
+++ src/constants/servicesName/authServicesName.js
@@ -3,3 +3,4 @@ export const LOGIN_SERVICE = 'Login service';
 export const GOOGLE_SERVICE = 'Google service';
 export const ACTIVATE_SERVICE = 'Activate service';
 export const REFRESH_ACTIVATE_SERVICE = 'Refresh activate service';
+export const AUTH_USER_SERVICE = 'Auth user service';

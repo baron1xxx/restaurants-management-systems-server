2c8f31afcad5ae63ffd17d2b4f217670ed98d4e8
rommyk <baron1xxx@gmail.com>
2020-07-25 14:26:39 +0300

    Add getById to UserRepository

--- src/data/repositories/userRepository.js
+++ src/data/repositories/userRepository.js
@@ -1,8 +1,24 @@
 import BaseRepository from './baseRepository';
-import { UserModel } from '../models/index';
+import { CredentialModel, ImageModel, RoleModel, UserModel } from '../models/index';
 
 class UserRepository extends BaseRepository {
-
+  getById(id) {
+    return this.model.findByPk(id, {
+      include: [
+        {
+          model: CredentialModel
+        },
+        {
+          model: ImageModel,
+          attributes: ['id', 'link']
+        },
+        {
+          model: RoleModel,
+          attributes: ['id', 'role']
+        }
+      ]
+    });
+  }
 }
 
 export default new UserRepository(UserModel);

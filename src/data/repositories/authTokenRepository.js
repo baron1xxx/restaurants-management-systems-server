711f1fab469d0f4f5937b71c40884f203fd90288
rommyk <baron1xxx@gmail.com>
2020-07-25 14:27:35 +0300

    Add getUserByAccessToken to AuthTokenRepository

--- src/data/repositories/authTokenRepository.js
+++ src/data/repositories/authTokenRepository.js
@@ -1,8 +1,17 @@
 import BaseRepository from './baseRepository';
-import { AuthTokenModel } from '../models';
+import { AuthTokenModel, UserModel } from '../models';
 
 class AuthTokenRepository extends BaseRepository {
-
+  getUserByAccessToken(accessToken) {
+    return this.model.findOne({
+      where: {
+        accessToken
+      },
+      include: {
+        model: UserModel
+      }
+    });
+  }
 }
 
 export default new AuthTokenRepository(AuthTokenModel);

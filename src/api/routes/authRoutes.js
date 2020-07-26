a2588402308c720c86ad63cf3e54a5fcbef4542f
rommyk <baron1xxx@gmail.com>
2020-07-26 19:02:04 +0300

    Add auth/password/forgot and auth/password/change:token router

--- src/api/routes/authRoutes.js
+++ src/api/routes/authRoutes.js
@@ -3,7 +3,10 @@ import userValidateMiddleware from '../middlewares/userMiddlewares/userValidateM
 import loginValidateMiddleware from '../middlewares/authMiddlewares/loginValidateMiddleware';
 import googleLoginValidateMiddleware from '../middlewares/authMiddlewares/googleLoginValidateMiddleware';
 import checkActivateTokenMiddleware from '../middlewares/authMiddlewares/checkActivateTokenMiddleware';
+import checkChangePasswordTokenMiddleware from '../middlewares/authMiddlewares/checkChangePasswordTokenMiddleware';
+import changePasswordValidateMiddleware from '../middlewares/authMiddlewares/changePasswordValidateMiddleware';
 import emailValidateMiddleware from '../middlewares/authMiddlewares/emailValidateMiddleware';
+import checkEmailMiddleware from '../middlewares/authMiddlewares/checkEmailMiddleware';
 import jwtAccessTokenMiddlewar from '../middlewares/authMiddlewares/jwtAccessTokenMiddlewar';
 import * as authController from '../controllers/authController';
 
@@ -16,7 +19,12 @@ router
   .post('/activate', emailValidateMiddleware, authController.refreshActivate)
   .post('/register', userValidateMiddleware, authController.register)
   .post('/login', loginValidateMiddleware, authController.login)
-  .post('/google', googleLoginValidateMiddleware, authController.google);
+  .post('/google', googleLoginValidateMiddleware, authController.google)
+  .post('/password/forgot', emailValidateMiddleware, checkEmailMiddleware, authController.forgotPassword)
+  .post('/password/change/:token',
+    checkChangePasswordTokenMiddleware,
+    changePasswordValidateMiddleware,
+    authController.changePassword);
 
 // Test passport google authentication
 

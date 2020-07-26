d587ccb15db408704da570d3d165ae2309e8cf9d
rommyk <baron1xxx@gmail.com>
2020-07-26 19:11:10 +0300

    Update emailTemplate (add forgotPasswordTemplate)

--- src/template/emailTemplate.js
+++ src/template/emailTemplate.js
@@ -14,3 +14,18 @@ export const activateAccountTemplate = token => (
       style="color: white" 
       href=${baseServerURL}/api/auth/activate/${token}>Follow this link to activate your account</a>
    </p>`);
+
+export const forgotPasswordTemplate = token => (
+  `<p style="
+                      background-color: #008CBA;
+                      border: none;
+                      padding: 15px 32px;
+                      text-align: center;
+                      text-decoration: none;
+                      display: inline-block;
+                      font-size: 16px;
+                      border-radius: 4px;">
+      <a 
+      style="color: white" 
+      href=${baseServerURL}/api/auth/password/change/${token}>Follow this link to change your password</a>
+   </p>`);

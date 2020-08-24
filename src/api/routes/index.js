6055ad5b67c4280dbf97bd9f12e2403100157489
rommyk <baron1xxx@gmail.com>
2020-08-24 15:01:51 +0300

    Add /address router

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -1,6 +1,8 @@
 import authRoutes from './authRoutes';
+import addressRoutes from './addressRoutes';
 
 // register all routes
 export default app => {
   app.use('/api/auth', authRoutes);
+  app.use('/api/address', addressRoutes);
 };

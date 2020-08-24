076cc8575b210bf7dd43fef464222fb793c6983a
rommyk <baron1xxx@gmail.com>
2020-08-24 15:43:39 +0300

    Add service getCitiesByRegionIdByName

--- src/api/services/addressService.js
+++ src/api/services/addressService.js
@@ -1,9 +1,18 @@
 import regionRepository from '../../data/repositories/regionRepository';
+import cityRepository from '../../data/repositories/cityRepository';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
 
 export const getRegionsByName = async region => {
   try {
     return await regionRepository.getAllByName(region);
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, e.controller);
+  }
+};
+
+export const getCitiesByRegionIdByName = async (regionId, city) => {
+  try {
+    return await cityRepository.getAllByRegionIdByName(regionId, city);
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, e.controller);
   }

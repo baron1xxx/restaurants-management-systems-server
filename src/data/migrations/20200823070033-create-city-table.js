eb4e4be6fcfa7e01e20b8d4bf570458bda6e29b0
rommyk <baron1xxx@gmail.com>
2020-08-24 15:36:32 +0300

    Add city migration

---
+++ src/data/migrations/20200823070033-create-city-table.js
@@ -0,0 +1,23 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(transaction => queryInterface.createTable('cities', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    }, {
+      transaction,
+      charset: 'utf8',
+      collate: 'utf8_unicode_ci'
+    })),
+  down: queryInterface => queryInterface.sequelize
+    .transaction(transaction => queryInterface.dropTable('cities', { transaction }))
+};

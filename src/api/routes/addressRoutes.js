7cc6f0ef52e86b529a2bf2e2b1254b69b4ee547d
rommyk <baron1xxx@gmail.com>
2020-08-25 19:15:00 +0300

    Add address/streets router

--- src/api/routes/addressRoutes.js
+++ src/api/routes/addressRoutes.js
@@ -5,6 +5,7 @@ const router = Router();
 
 router
   .get('/regions', addressController.getRegionsByName)
-  .get('/cities', addressController.getCitiesByRegionIdByName);
+  .get('/cities', addressController.getCitiesByRegionIdByName)
+  .get('/streets', addressController.getStreetsByCityIdByName);
 
 export default router;

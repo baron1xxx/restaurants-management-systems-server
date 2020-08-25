3dca08591acd08e04ffa37b33478071dbdbb0b56
rommyk <baron1xxx@gmail.com>
2020-08-25 19:12:22 +0300

    Add getStreetsByCityIdByName() method to the address service

--- src/api/services/addressService.js
+++ src/api/services/addressService.js
@@ -1,5 +1,6 @@
 import regionRepository from '../../data/repositories/regionRepository';
 import cityRepository from '../../data/repositories/cityRepository';
+import streetRepository from '../../data/repositories/streetRepository';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
 
 export const getRegionsByName = async region => {
@@ -13,6 +14,14 @@ export const getRegionsByName = async region => {
 export const getCitiesByRegionIdByName = async (regionId, city) => {
   try {
     return await cityRepository.getAllByRegionIdByName(regionId, city);
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, e.controller);
+  }
+};
+
+export const getStreetsByCityIdByName = async (cityId, street) => {
+  try {
+    return await streetRepository.getAllByCityIdByName(cityId, street);
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, e.controller);
   }

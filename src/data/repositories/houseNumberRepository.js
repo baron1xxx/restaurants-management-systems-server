4c5534f78ffd93b3fa0064ec700a3c27b2b9d224
rommyk <baron1xxx@gmail.com>
2020-08-25 19:35:42 +0300

    Add houseNumber repository

---
+++ src/data/repositories/houseNumberRepository.js
@@ -0,0 +1,8 @@
+import BaseRepository from './baseRepository';
+import { HouseNumberModel } from '../models/index';
+
+class HouseNumberRepository extends BaseRepository {
+
+}
+
+export default new HouseNumberRepository(HouseNumberModel);

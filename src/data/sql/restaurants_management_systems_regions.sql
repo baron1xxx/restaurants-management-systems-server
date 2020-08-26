189307173648e14b3360cc758e4bd9d4fc4a3e4b
rommyk <baron1xxx@gmail.com>
2020-08-26 21:52:21 +0300

    Add address sql damp files

---
+++ src/data/sql/restaurants_management_systems_regions.sql
@@ -0,0 +1,35 @@
+create table regions
+(
+    id        int auto_increment
+        primary key,
+    name      varchar(255) not null,
+    createdAt datetime     null,
+    updatedAt datetime     null
+)
+    collate = utf8_unicode_ci;
+
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (1, 'Автономна Республіка Крим', '2020-08-23 07:55:46', '2020-08-23 07:55:46');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (2, 'Вінницька обл.', '2020-08-23 07:56:24', '2020-08-23 07:56:24');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (3, 'Волинська обл.', '2020-08-23 07:57:01', '2020-08-23 07:57:01');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (4, 'Дніпропетровська обл.', '2020-08-23 07:57:41', '2020-08-23 07:57:41');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (5, 'Донецька обл.', '2020-08-23 07:58:34', '2020-08-23 07:58:34');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (6, 'Житомирська обл.', '2020-08-23 07:59:30', '2020-08-23 07:59:30');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (7, 'Закарпатська обл.', '2020-08-23 08:00:00', '2020-08-23 08:00:00');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (8, 'Запорізька обл.', '2020-08-23 08:00:16', '2020-08-23 08:00:16');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (9, 'Івано-Франківська обл.', '2020-08-23 08:00:42', '2020-08-23 08:00:42');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (10, 'Київська обл.', '2020-08-23 08:01:12', '2020-08-23 08:01:12');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (11, 'Кіровоградська обл.', '2020-08-23 08:02:13', '2020-08-23 08:02:13');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (12, 'Луганська обл.', '2020-08-23 08:02:47', '2020-08-23 08:02:47');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (13, 'Львівська обл.', '2020-08-23 08:03:25', '2020-08-23 08:03:25');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (14, 'Миколаївська обл.', '2020-08-23 08:04:08', '2020-08-23 08:04:08');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (15, 'Одеська обл.', '2020-08-23 08:04:36', '2020-08-23 08:04:36');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (16, 'Полтавська обл.', '2020-08-23 08:05:09', '2020-08-23 08:05:09');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (17, 'Рівненська обл.', '2020-08-23 08:05:45', '2020-08-23 08:05:45');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (18, 'Сумська обл.', '2020-08-23 08:06:23', '2020-08-23 08:06:23');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (19, 'Тернопільська обл.', '2020-08-23 08:06:45', '2020-08-23 08:06:45');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (20, 'Харківська обл.', '2020-08-23 08:07:07', '2020-08-23 08:07:07');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (21, 'Херсонська обл.', '2020-08-23 08:08:02', '2020-08-23 08:08:02');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (22, 'Хмельницька обл.', '2020-08-23 08:08:17', '2020-08-23 08:08:17');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (23, 'Черкаська обл.', '2020-08-23 08:08:50', '2020-08-23 08:08:50');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (24, 'Чернівецька обл.', '2020-08-23 08:09:15', '2020-08-23 08:09:15');
+INSERT INTO `restaurants-management-systems`.regions (id, name, createdAt, updatedAt) VALUES (25, 'Чернігівська обл.', '2020-08-23 08:09:50', '2020-08-23 08:09:50');

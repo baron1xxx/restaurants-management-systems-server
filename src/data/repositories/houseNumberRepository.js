b0054aadad38c1099dec0b975087d74b7eca72ed
rommyk <baron1xxx@gmail.com>
2020-09-01 15:05:08 +0300

    Add check address middleware

--- src/data/repositories/houseNumberRepository.js
+++ src/data/repositories/houseNumberRepository.js
@@ -2,7 +2,12 @@ import BaseRepository from './baseRepository';
 import { HouseNumberModel } from '../models/index';
 
 class HouseNumberRepository extends BaseRepository {
-
+  findOrCreate(findObj, data) {
+    return this.findOrCreate({
+      where: findObj,
+      defaults: data
+    });
+  }
 }
 
 export default new HouseNumberRepository(HouseNumberModel);

f5580f98d9780626be0d754d45975b6c8e94c210
rommyk <baron1xxx@gmail.com>
2020-09-21 10:29:50 +0300

    Add getAll. count and create include variable

--- src/data/repositories/restaurantRepository.js
+++ src/data/repositories/restaurantRepository.js
@@ -10,46 +10,61 @@ import {
   OpeningModel,
   ImageModel
 } from '../models/index';
+import { LIMIT, OFFSET } from '../../constants/paginationConstants';
+
+const include = [
+  {
+    model: GeolocationModel,
+    attributes: ['id', 'latitude', 'longitude']
+  },
+  {
+    model: AddressModel,
+    include: [
+      {
+        model: RegionModel,
+        attributes: ['id', 'name']
+      },
+      {
+        model: CityModel,
+        attributes: ['id', 'name']
+      },
+      {
+        model: StreetModel,
+        attributes: ['id', 'name']
+      },
+      {
+        model: HouseNumberModel,
+        attributes: ['id', 'number']
+      }
+    ]
+  },
+  {
+    model: OpeningModel,
+    attributes: ['id', 'day', 'start', 'end']
+  },
+  {
+    model: ImageModel,
+    attributes: ['id', 'link']
+  }
+];
 
 class RestaurantRepository extends BaseRepository {
   getById(id) {
     return this.model.findByPk(id, {
-      include: [
-        {
-          model: GeolocationModel,
-          attributes: ['id', 'latitude', 'longitude']
-        },
-        {
-          model: AddressModel,
-          include: [
-            {
-              model: RegionModel,
-              attributes: ['id', 'name']
-            },
-            {
-              model: CityModel,
-              attributes: ['id', 'name']
-            },
-            {
-              model: StreetModel,
-              attributes: ['id', 'name']
-            },
-            {
-              model: HouseNumberModel,
-              attributes: ['id', 'number']
-            }
-          ]
-        },
-        {
-          model: OpeningModel,
-          attributes: ['id', 'day', 'start', 'end']
-        },
-        {
-          model: ImageModel,
-          attributes: ['id', 'link']
-        }
-      ]
+      include
     });
+  }
+
+  getAll(limit = LIMIT, offset = OFFSET) {
+    return this.model.findAll({
+      include,
+      limit,
+      offset
+    });
+  }
+
+  countAll() {
+    return this.model.count();
   }
 }
 

fdb331cabc6c5f8fc098dfdeb40afd8803b1bd46
baron1xxx <baron1xxx@gmail.con>
2020-09-23 17:16:54 +0300

    Add update and delete restaurant controller. Other controller fix

--- src/api/controllers/restaurantController.js
+++ src/api/controllers/restaurantController.js
@@ -25,9 +25,25 @@ export const create = async (req, res, next) => {
 
 export const getRestaurants = async (req, res, next) => {
   try {
-    const { pagination } = req;
+    const { pagination, query } = req;
 
-    const restaurants = await restaurantService.getRestaurants({ ...pagination });
+    const restaurants = await restaurantService.getRestaurants({ ...query, ...pagination });
+
+    res.status(200)
+      .json({
+        error: false,
+        data: restaurants
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
+
+export const getRestaurantsByName = async (req, res, next) => {
+  try {
+    const { pagination, query } = req;
+
+    const restaurants = await restaurantService.getRestaurants({ ...query, ...pagination });
 
     res.status(200)
       .json({
@@ -41,14 +57,62 @@ export const getRestaurants = async (req, res, next) => {
 
 export const getRestaurantsByUserId = async (req, res, next) => {
   try {
-    const { pagination, user: { id: userId } } = req;
+    const { pagination, user: { id: userId }, query } = req;
 
-    const restaurants = await restaurantService.getRestaurants({ ...pagination, userId });
+    const restaurants = await restaurantService.getRestaurants({ ...query, ...pagination, userId });
 
     res.status(200)
       .json({
         error: false,
         data: restaurants
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
+
+export const getById = async (req, res, next) => {
+  try {
+    const { params: { id } } = req;
+
+    const restaurant = await restaurantService.getById(id);
+
+    res.status(200)
+      .json({
+        error: false,
+        data: restaurant
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
+
+export const updateRestaurant = async (req, res, next) => {
+  try {
+    const { params: { id }, user: { id: userId }, body } = req;
+
+    const restaurantUpdated = await restaurantService.update({ id, userId }, body);
+
+    res.status(200)
+      .json({
+        error: false,
+        data: restaurantUpdated
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
+
+export const removeRestaurant = async (req, res, next) => {
+  try {
+    const { params: { id }, user: { id: userId } } = req;
+
+    const restaurantDeleted = await restaurantService.remove({ id, userId });
+
+    res.status(200)
+      .json({
+        error: false,
+        data: restaurantDeleted
       });
   } catch (e) {
     next(new ErrorHandler(e.status, e.message, e.controller));

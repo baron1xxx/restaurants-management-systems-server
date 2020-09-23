816f5ae2c2742f5096906fdb3453d6d3366498ca
baron1xxx <baron1xxx@gmail.con>
2020-09-23 17:17:46 +0300

    Add update and delete restaurant service. Other service fix

--- src/api/services/restaurantService.js
+++ src/api/services/restaurantService.js
@@ -8,6 +8,9 @@ import { addressObjToString } from '../../helpers/addressObjToString';
 import { countPages, offset } from '../../helpers/paginationHelper';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
 import { LIMIT, PAGE } from '../../constants/paginationConstants';
+import { restaurantErrorMessages } from '../../constants/customErrorMessage/restaurantErrorMessage';
+import { restaurantSuccessMessage } from '../../constants/customSuccessMessage/restaurantSuccessMessage';
+import { NOT_FOUND } from '../../constants/responseStatusCodes';
 
 export const create = async (address, restaurantData, opening, file, userId) => {
   try {
@@ -38,14 +41,22 @@ export const create = async (address, restaurantData, opening, file, userId) => 
 
     return restaurantRepository.getById(restaurantId);
   } catch (e) {
-    throw new ErrorHandler(e.status, e.message, e.message);
+    throw new ErrorHandler(
+      e.status,
+      e.message,
+      'Restaurant service create()'
+    );
   }
 };
 
 export const getRestaurants = async filter => {
   try {
     const { limit = LIMIT, page = PAGE } = filter;
-    const restaurantsCount = await restaurantRepository.countAll();
+    console.log('**********************');
+    console.log(filter);
+    console.log('**********************');
+    const restaurantsCount = await restaurantRepository.countAll(filter);
+    console.log(restaurantsCount);
 
     return {
       restaurants: await restaurantRepository.getAll({
@@ -54,14 +65,58 @@ export const getRestaurants = async filter => {
       totalPage: countPages(restaurantsCount, limit)
     };
   } catch (e) {
-    throw new ErrorHandler(e.status, e.message, 'Restaurant service geAll()');
+    throw new ErrorHandler(
+      e.status,
+      e.message,
+      'Restaurant service geAll()'
+    );
   }
 };
 
 export const getById = async id => {
   try {
-    return await restaurantRepository.getById(id);
+    const restaurant = await restaurantRepository.getById(id);
+    if (!restaurant) {
+      throw new ErrorHandler(
+        NOT_FOUND,
+        restaurantErrorMessages.RESTAURANT_NOT_FOUND,
+        'Restaurant getById()'
+      );
+    }
+    return restaurant;
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, 'Restaurant service getById()');
+  }
+};
+
+export const update = async (find, data) => {
+  try {
+    const restaurantUpdated = await restaurantRepository.updateById(find, data);
+    if (!restaurantUpdated) {
+      throw new ErrorHandler(
+        NOT_FOUND,
+        restaurantErrorMessages.RESTAURANT_NOT_FOUND,
+        'Restaurant update()'
+      );
+    }
+    return restaurantSuccessMessage.RESTAURANT_SUCCESS_UPDATED;
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, 'Restaurant service getById()');
+  }
+};
+
+export const remove = async find => {
+  try {
+    const restaurantDeleted = await restaurantRepository.updateById(find, { isDeleted: true });
+    if (!restaurantDeleted) {
+      throw new ErrorHandler(
+        NOT_FOUND,
+        restaurantErrorMessages.RESTAURANT_NOT_FOUND,
+        'Restaurant delete()'
+      );
+    }
+    return restaurantSuccessMessage.RESTAURANT_SUCCESS_DELETED;
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, e.controller);
   }
 };

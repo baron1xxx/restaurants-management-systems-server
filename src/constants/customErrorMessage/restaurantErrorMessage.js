6e2ffd028f6cd55650f2ff37be4aa38f6b073c5a
baron1xxx <baron1xxx@gmail.con>
2020-09-23 17:19:03 +0300

    Add error and success messages to constants

--- src/constants/customErrorMessage/restaurantErrorMessage.js
+++ src/constants/customErrorMessage/restaurantErrorMessage.js
@@ -1,3 +1,4 @@
 export const restaurantErrorMessages = {
-  RESTAURANT_EXISTS: 'This restaurant already exists'
+  RESTAURANT_EXISTS: 'This restaurant already exists',
+  RESTAURANT_NOT_FOUND: 'Restaurant not found'
 };

a58c6c7d07d4c8ce27550ac7ef21d10173e74fd0
rommyk <baron1xxx@gmail.com>
2020-09-28 10:15:39 +0300

    Add getById, update and delete functions to imageService

--- src/api/services/imageService.js
+++ src/api/services/imageService.js
@@ -1,6 +1,9 @@
 import axios from 'axios';
 import { imgurId } from '../../config/imgurConfig';
 import imageRepository from '../../data/repositories/imageRepository';
+import { ErrorHandler } from '../../helpers/error/ErrorHandler';
+import { NOT_FOUND } from '../../constants/responseStatusCodes';
+import { imageErrorMessages } from '../../constants/customErrorMessage/imageErrorMessage';
 
 const uploadToImgur = async file => {
   try {
@@ -23,7 +26,47 @@ const uploadToImgur = async file => {
   }
 };
 
+// eslint-disable-next-line no-unused-vars
+const deleteFromImgur = async deleteHash => {
+  try {
+    const { data: { data } } = await axios.post(
+      `https://api.imgur.com/3/image/${deleteHash}`,
+      {},
+      {
+        headers: { Authorization: `Client-ID ${imgurId}` }
+      }
+    );
+    // eslint-disable-next-line no-console
+    console.log('data - ', data);
+    return data;
+  } catch ({ response: { data: { status, data } } }) { // parse Imgur error
+    return Promise.reject({ status, message: data.error });
+  }
+};
+
 export const upload = async file => {
   const image = await uploadToImgur(file);
   return imageRepository.create(image);
+};
+
+export const update = async (id, deleteHash, file) => {
+  await deleteFromImgur(deleteHash);
+  const image = await uploadToImgur(file);
+  return imageRepository.updateById(id, image);
+};
+
+export const getById = async id => {
+  try {
+    const image = await imageRepository.getById(id);
+    if (!image) {
+      throw new ErrorHandler(
+        NOT_FOUND,
+        imageErrorMessages.IMAGE_NOT_EXITS,
+        'Image getById()'
+      );
+    }
+    return image;
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, 'Restaurant service getById()');
+  }
 };

77b79a6cdfe84d028b3cbae9893ddf8d9fbd8042
rommyk <baron1xxx@gmail.com>
2020-09-28 10:13:55 +0300

    Add new constants to customErrorMessage

--- src/constants/customErrorMessage/restaurantErrorMessage.js
+++ src/constants/customErrorMessage/restaurantErrorMessage.js
@@ -1,4 +1,6 @@
 export const restaurantErrorMessages = {
   RESTAURANT_EXISTS: 'This restaurant already exists',
-  RESTAURANT_NOT_FOUND: 'Restaurant not found'
+  RESTAURANT_NOT_FOUND: 'Restaurant not found',
+  RESTAURANT_NOT_UPDATED: 'Restaurant not updated',
+  OWNER_OR_ADMIN_CAN_UPDATE_RESTAURANT: 'Only the owner or administrator can update the restaurant'
 };

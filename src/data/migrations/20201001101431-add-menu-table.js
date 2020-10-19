c2ae37009692a68ef656dfd4d8f1d5cea0ab5d97
rommyk <baron1xxx@gmail.com>
2020-10-19 10:43:32 +0300

    Add menu table migration

---
+++ src/data/migrations/20201001101431-add-menu-table.js
@@ -0,0 +1,24 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(transaction => queryInterface.createTable('menus', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      isDeleted: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    }, { transaction })),
+  down: queryInterface => queryInterface.sequelize
+    .transaction(transaction => queryInterface.dropTable('menus', { transaction }))
+};

40d666e8cdc078a96c38f6f5533cc4432c3d1401
baron1xxx <baron1xxx@gmail.com>
2020-10-19 16:58:34 +0300

    Add dish table migration

---
+++ src/data/migrations/20201019095911-add-dish-table.js
@@ -0,0 +1,32 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(transaction => queryInterface.createTable('dishes', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      name: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      description: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      price: {
+        type: Sequelize.FLOAT,
+        allowNull: false
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    }, { transaction })),
+  down: queryInterface => queryInterface.sequelize
+    .transaction(transaction => queryInterface.dropTable('dishes', { transaction }))
+};

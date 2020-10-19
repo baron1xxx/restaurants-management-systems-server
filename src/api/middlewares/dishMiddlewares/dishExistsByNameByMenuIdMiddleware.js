17533178104db4c56be4a54410d772aa52d2790c
rommyk <baron1xxx@gmail.com>
2020-10-19 23:40:07 +0300

    Add dishExistsByNameByMenuIdMiddleware

---
+++ src/api/middlewares/dishMiddlewares/dishExistsByNameByMenuIdMiddleware.js
@@ -0,0 +1,28 @@
+import * as menuService from '../../services/menuService';
+import dishRepository from '../../../data/repositories/dishRepository';
+import { ErrorHandler } from '../../../helpers/error/ErrorHandler';
+import { dishErrorMessages } from '../../../constants/customErrorMessage/dishErrorMessage';
+import { BEAD_REQUEST } from '../../../constants/responseStatusCodes';
+
+// eslint-disable-next-line consistent-return
+export default async (req, res, next) => {
+  try {
+    const { body: { name, menuId } } = req;
+    // Check if menu exists.
+    await menuService.getById(menuId);
+    // TODO Якщо є імя то перевіряти на унікальність!!! if (name) {ПЕРЕВІРИТИ}
+    const dishExists = name
+      ? await dishRepository.getOne({ name, menuId })
+      : null;
+
+    return !dishExists
+      ? next()
+      : next(new ErrorHandler(
+        BEAD_REQUEST,
+        dishErrorMessages.DISH_EXISTS,
+        'Check if dish exists by name middleware'
+      ));
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
--- src/api/routes/dishesRoutes.js
+++ src/api/routes/dishesRoutes.js
@@ -1,6 +1,7 @@
 import { Router } from 'express';
 import jwtAccessTokenMiddleware from '../middlewares/authMiddlewares/jwtAccessTokenMiddleware';
 import createDishValidMiddleware from '../middlewares/dishMiddlewares/createDishValidMiddleware';
+import dishExistsByNameByMenuIdMiddleware from '../middlewares/dishMiddlewares/dishExistsByNameByMenuIdMiddleware';
 import imageMiddleware from '../middlewares/imageMiddlewares/imageMiddleware';
 import * as dishController from '../controllers/dishController';
 
@@ -11,6 +12,7 @@ router
     jwtAccessTokenMiddleware,
     imageMiddleware,
     createDishValidMiddleware,
+    dishExistsByNameByMenuIdMiddleware,
     dishController.create);
 
 export default router;

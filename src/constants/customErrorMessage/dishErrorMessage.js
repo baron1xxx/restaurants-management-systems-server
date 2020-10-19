5ae4618e69f52988a57b8d9e16baeab967ba12d0
baron1xxx <baron1xxx@gmail.com>
2020-10-19 16:54:50 +0300

    Add dish error message

---
+++ src/constants/customErrorMessage/dishErrorMessage.js
@@ -0,0 +1,6 @@
+export const dishErrorMessages = {
+  DISH_EXISTS: 'This dish already exists',
+  DISH_NOT_FOUND: 'Dish not found',
+  DISH_NOT_UPDATED: 'Dish not updated',
+  OWNER_OR_ADMIN_CAN_UPDATE_DISH: 'Only the owner or administrator can update the dish'
+};

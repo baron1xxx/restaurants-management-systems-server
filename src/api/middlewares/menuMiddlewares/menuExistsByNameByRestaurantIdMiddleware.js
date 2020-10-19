b608b150a98ad6c5cc400de756da7b4f410d0be7
rommyk <baron1xxx@gmail.com>
2020-10-19 10:41:40 +0300

    Add menu exists by name by restaurantId middleware

---
+++ src/api/middlewares/menuMiddlewares/menuExistsByNameByRestaurantIdMiddleware.js
@@ -0,0 +1,28 @@
+import * as restaurantService from '../../services/restaurantService';
+import menuRepository from '../../../data/repositories/menuRepository';
+import { ErrorHandler } from '../../../helpers/error/ErrorHandler';
+import { menuErrorMessages } from '../../../constants/customErrorMessage/menuErrorMessage';
+import { BEAD_REQUEST } from '../../../constants/responseStatusCodes';
+
+// eslint-disable-next-line consistent-return
+export default async (req, res, next) => {
+  try {
+    const { body: { name, restaurantId } } = req;
+    // Check if restaurant exists.
+    await restaurantService.getById(restaurantId);
+    // TODO Якщо є імя то перевіряти на унікальність!!! if (name) {ПЕРЕВІРИТИ}
+    const menuExists = name
+      ? await menuRepository.getOne({ name, restaurantId })
+      : null;
+
+    return !menuExists
+      ? next()
+      : next(new ErrorHandler(
+        BEAD_REQUEST,
+        menuErrorMessages.MENU_EXISTS,
+        'Check if menu exists by name middleware'
+      ));
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};

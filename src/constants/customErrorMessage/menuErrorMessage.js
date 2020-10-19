37a07aeb5815e38ab3de48c25193288cd0bb1848
rommyk <baron1xxx@gmail.com>
2020-10-19 10:53:47 +0300

    Add menu error messages

---
+++ src/constants/customErrorMessage/menuErrorMessage.js
@@ -0,0 +1,6 @@
+export const menuErrorMessages = {
+  MENU_EXISTS: 'This menu already exists',
+  MENU_NOT_FOUND: 'Menu not found',
+  MENU_NOT_UPDATED: 'Menu not updated',
+  OWNER_OR_ADMIN_CAN_UPDATE_MENU: 'Only the owner or administrator can update the menu'
+};

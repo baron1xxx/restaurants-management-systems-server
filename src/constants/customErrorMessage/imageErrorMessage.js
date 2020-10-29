e419c52cf3c56b9727b917a052a318d7993a6d57
rommyk <baron1xxx@gmail.com>
2020-10-29 19:48:47 +0200

    Add custom error message to imageErrorMessages

--- src/constants/customErrorMessage/imageErrorMessage.js
+++ src/constants/customErrorMessage/imageErrorMessage.js
@@ -2,5 +2,6 @@ export const imageErrorMessages = {
   IMAGE_NOT_EXITS: 'Image not exist.',
   FILE_NOT_EXITS: 'File not exist.',
   NOT_APPROPRIATE_FILE_TYPE: 'Not appropriate file type.',
-  LARGE_FILE_SIZE: 'File size exceeds 5 MB.'
+  LARGE_FILE_SIZE: 'File size exceeds 5 MB.',
+  IMAGE_IS_REQUIRED: 'Image is required.'
 };

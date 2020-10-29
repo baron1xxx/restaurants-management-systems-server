acb2ff1b9f4cf36fcdd81fbf23204024b9113587
rommyk <baron1xxx@gmail.com>
2020-10-29 19:51:39 +0200

    Added update dish function. Added check the file exists.

--- src/api/services/dishService.js
+++ src/api/services/dishService.js
@@ -2,20 +2,27 @@ import * as menuService from './menuService';
 import * as imageService from './imageService';
 import dishRepository from '../../data/repositories/dishRepository';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
-// import menuRepository from '../../data/repositories/menuRepository';
-import { NOT_FOUND } from '../../constants/responseStatusCodes';
+import { FORBIDDEN, NOT_FOUND } from '../../constants/responseStatusCodes';
 import { dishErrorMessages } from '../../constants/customErrorMessage/dishErrorMessage';
+import { imageErrorMessages } from '../../constants/customErrorMessage/imageErrorMessage';
 import { LIMIT, PAGE } from '../../constants/paginationConstants';
 import { countPages, offset } from '../../helpers/paginationHelper';
+import { roles } from '../../constants/roles';
+import { menuErrorMessages } from '../../constants/customErrorMessage/menuErrorMessage';
 
 export const create = async ({ menuId, file, ...dishData }) => {
   try {
     // Check if menu exists
     await menuService.getById(menuId);
-    // If file exist upload else return empty object and assign default value to imageId = null
-    const { id: imageId = null } = file
-      ? await imageService.upload(file)
-      : {};
+    // Check if file exists
+    if (!file) {
+      throw new ErrorHandler(
+        NOT_FOUND,
+        imageErrorMessages.IMAGE_IS_REQUIRED,
+        'Dish create()'
+      );
+    }
+    const { id: imageId = null } = await imageService.upload(file);
 
     const { id } = await dishRepository.create({ ...dishData, imageId, menuId });// TODO { imageId }
     const dish = await dishRepository.getById(id);
@@ -57,5 +64,28 @@ export const getDishes = async filter => {
     };
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, 'Dish service geAll()');
+  }
+};
+
+export const update = async (id, data) => {
+  try {
+    const { file, user, ...dishBody } = data;
+    const dish = await getById(id);
+
+    if (dish.menu.restaurant.userId !== user.id && user.role.role !== roles.ADMIN) {
+      throw new ErrorHandler(
+        FORBIDDEN,
+        menuErrorMessages.OWNER_OR_ADMIN_CAN_UPDATE_MENU,
+        'Restaurant update() getById()'
+      );
+    }
+    if (file) {
+      await imageService.update(dish.image.id, file);
+    }
+
+    await dishRepository.updateById(id, dishBody);
+    return await dishRepository.getById(id);
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, 'Dish service update()');
   }
 };

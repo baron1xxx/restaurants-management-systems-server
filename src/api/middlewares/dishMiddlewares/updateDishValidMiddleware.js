4ddf0c65d6c6153b37dacbdf92fb0adff8de3735
rommyk <baron1xxx@gmail.com>
2020-10-29 19:55:22 +0200

    Added updateDishValidMiddleware

---
+++ src/api/middlewares/dishMiddlewares/updateDishValidMiddleware.js
@@ -0,0 +1,22 @@
+import { ErrorHandler } from '../../../helpers/error/ErrorHandler';
+import { dishUpdateValidator } from '../../../validators/dishValidator';
+import { BEAD_REQUEST } from '../../../constants/responseStatusCodes';
+
+// eslint-disable-next-line consistent-return
+export default (req, res, next) => {
+  try {
+    const { error } = dishUpdateValidator.validate(req.body);
+    if (error) {
+      return next(
+        new ErrorHandler(
+          BEAD_REQUEST,
+          error.details[0].message,
+          'createDishValidMiddleware'
+        )
+      );
+    }
+    next();
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};

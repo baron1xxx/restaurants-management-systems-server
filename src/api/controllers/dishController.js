683183f5327d00bcaacb74fc3806c1be02b165b8
rommyk <baron1xxx@gmail.com>
2020-10-29 19:57:09 +0200

    Added updateDish and removeDish function.

--- src/api/controllers/dishController.js
+++ src/api/controllers/dishController.js
@@ -48,3 +48,40 @@ export const getDishes = async (req, res, next) => {
     next(new ErrorHandler(e.status, e.message, e.controller));
   }
 };
+
+export const updateDish = async (req, res, next) => {
+  try {
+    const {
+      body,
+      file,
+      user,
+      params: { id }
+    } = req;
+
+    const dishUpdated = await dishService.update(id, { ...body, file, user });
+
+    res.status(200)
+      .json({
+        error: false,
+        data: dishUpdated
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};
+
+export const removeDish = async (req, res, next) => {
+  try {
+    const { params: { id }, user } = req;
+
+    const dishDeleted = await dishService.update(id, { user, isDeleted: true });
+
+    res.status(200)
+      .json({
+        error: false,
+        data: dishDeleted
+      });
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};

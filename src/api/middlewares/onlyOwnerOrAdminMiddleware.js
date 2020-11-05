3c6cdd4e52a76ad24e43d28874e1c717f757681d
rommyk <baron1xxx@gmail.com>
2020-11-05 11:26:02 +0200

    Added only OWNER or ADMIN middleware

---
+++ src/api/middlewares/onlyOwnerOrAdminMiddleware.js
@@ -0,0 +1,24 @@
+import * as tableService from '../services/tableService';
+import { ErrorHandler } from '../../helpers/error/ErrorHandler';
+import { UNAUTHORIZED } from '../../constants/responseStatusCodes';
+import { authErrorMessages } from '../../constants/customErrorMessage/authErrorMessage';
+import { roles } from '../../constants/roles';
+
+// eslint-disable-next-line consistent-return
+export default async (req, res, next) => {
+  try {
+    const { params: { id: tableId }, user } = req;
+    // Check if table exists.
+    const table = await tableService.getById(tableId);
+    // Check if authentication user is owner restaurant.
+    return table.restaurant.userId === user.id || user.role.role === roles.ADMIN
+      ? next()
+      : next(new ErrorHandler(
+        UNAUTHORIZED,
+        authErrorMessages.UNAUTHORIZED,
+        'Only OWNER or ADMIN middleware'
+      ));
+  } catch (e) {
+    next(new ErrorHandler(e.status, e.message, e.controller));
+  }
+};

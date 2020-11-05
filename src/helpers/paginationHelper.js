24723d14945503f9470c6e1a10dd16eb418b9446
rommyk <baron1xxx@gmail.com>
2020-11-05 11:16:30 +0200

    Fix pagination helper (404 page not found)

--- src/helpers/paginationHelper.js
+++ src/helpers/paginationHelper.js
@@ -1,2 +1,14 @@
-export const countPages = (count, limit) => Math.ceil(count / limit);
+import { ErrorHandler } from './error/ErrorHandler';
+import { NOT_FOUND } from '../constants/responseStatusCodes';
+
+export const countPages = (count, limit, page) => {
+  try {
+    const totalPage = Math.ceil(count / limit);
+    if (totalPage < page) throw new ErrorHandler(NOT_FOUND, 'Page not found', 'Pagination helper');
+    return totalPage;
+  } catch (e) {
+    throw new ErrorHandler(e.status, e.message, 'Table service geAll()');
+  }
+};
+
 export const offset = (page, limit) => limit * (page - 1);

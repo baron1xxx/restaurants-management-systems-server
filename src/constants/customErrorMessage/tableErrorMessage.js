1ff767a77f2a12ad4af7213630eaad3c77920f6b
rommyk <baron1xxx@gmail.com>
2020-11-05 11:22:42 +0200

    Added table error message

---
+++ src/constants/customErrorMessage/tableErrorMessage.js
@@ -0,0 +1,6 @@
+export const tableErrorMessages = {
+  TABLE_EXISTS: 'This table already exists',
+  TABLE_NOT_FOUND: 'Table not found',
+  TABLE_NOT_UPDATED: 'Table not updated',
+  OWNER_OR_ADMIN_CAN_UPDATE_TABLE: 'Only the owner or administrator can update the table'
+};

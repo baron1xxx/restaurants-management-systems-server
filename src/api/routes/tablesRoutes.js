bad88992f2780539e70ef5c90fed73cb8a9d6b1c
baron1xxx <baron1xxx@gmail.com>
2020-11-18 15:27:23 +0200

    Fix tables router

--- src/api/routes/tablesRoutes.js
+++ src/api/routes/tablesRoutes.js
@@ -2,7 +2,7 @@ import { Router } from 'express';
 import * as tableController from '../controllers/tableController';
 import jwtAccessTokenMiddleware from '../middlewares/authMiddlewares/jwtAccessTokenMiddleware';
 import roleAuthorizationMiddleware from '../middlewares/authMiddlewares/roleAuthorizationMiddleware';
-import onlyOwnerOrAdminMiddleware from '../middlewares/onlyOwnerOrAdminMiddleware';
+import onlyRestaurantOwnerOrAdminMiddleware from '../middlewares/onlyRestaurantOwnerOrAdminMiddleware';
 import createTableValidMiddleware from '../middlewares/tableMiddlewares/createTableValidMiddleware';
 import updateTableValidMiddleware from '../middlewares/tableMiddlewares/updateTableValidMiddleware';
 import tableExistsByNumberByRestaurantIdMiddleware
@@ -13,30 +13,33 @@ import { roles } from '../../constants/roles';
 const router = Router();
 
 router
-  .get('/:tableId',
-    jwtAccessTokenMiddleware,
-    tableController.getById)
   .get('/byRestaurant/:restaurantId',
     paginationValidateMiddleware,
     tableController.getTables)
   .post('/',
     jwtAccessTokenMiddleware,
     roleAuthorizationMiddleware([roles.ADMIN, roles.OWNER]),
-    onlyOwnerOrAdminMiddleware,
+    onlyRestaurantOwnerOrAdminMiddleware,
     createTableValidMiddleware,
     tableExistsByNumberByRestaurantIdMiddleware,
     tableController.create)
-  .put('/:tableId',
+  .route('/:tableId')
+  .get(
+    jwtAccessTokenMiddleware,
+    tableController.getById
+  )
+  .all(
     jwtAccessTokenMiddleware,
     roleAuthorizationMiddleware([roles.ADMIN, roles.OWNER]),
+    onlyRestaurantOwnerOrAdminMiddleware
+  )
+  .put(
     updateTableValidMiddleware,
-    onlyOwnerOrAdminMiddleware,
     tableExistsByNumberByRestaurantIdMiddleware,
-    tableController.updateTable)
-  .delete('/:tableId',
-    jwtAccessTokenMiddleware,
-    roleAuthorizationMiddleware([roles.ADMIN, roles.OWNER]),
-    onlyOwnerOrAdminMiddleware,
-    tableController.removeTable);
+    tableController.updateTable
+  )
+  .delete(
+    tableController.removeTable
+  );
 
 export default router;

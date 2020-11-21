8f5b046a79bb24a9cdc3837154b09a4e246cec09
rommyk <baron1xxx@gmail.com>
2020-11-21 13:57:45 +0200

    Add dishService (getDishes, create, update)

--- src/api/services/dishService.js
+++ src/api/services/dishService.js
@@ -2,18 +2,14 @@ import * as menuService from './menuService';
 import * as imageService from './imageService';
 import dishRepository from '../../data/repositories/dishRepository';
 import { ErrorHandler } from '../../helpers/error/ErrorHandler';
-import { FORBIDDEN, NOT_FOUND } from '../../constants/responseStatusCodes';
+import { NOT_FOUND } from '../../constants/responseStatusCodes';
 import { dishErrorMessages } from '../../constants/customErrorMessage/dishErrorMessage';
 import { imageErrorMessages } from '../../constants/customErrorMessage/imageErrorMessage';
 import { LIMIT, PAGE } from '../../constants/paginationConstants';
 import { countPages, offset } from '../../helpers/paginationHelper';
-import { roles } from '../../constants/roles';
-import { menuErrorMessages } from '../../constants/customErrorMessage/menuErrorMessage';
 
 export const create = async ({ menuId, file, ...dishData }) => {
   try {
-    // Check if menu exists
-    await menuService.getById(menuId);
     // Check if file exists
     if (!file) {
       throw new ErrorHandler(
@@ -24,7 +20,7 @@ export const create = async ({ menuId, file, ...dishData }) => {
     }
     const { id: imageId = null } = await imageService.upload(file);
 
-    const { id } = await dishRepository.create({ ...dishData, imageId, menuId });// TODO { imageId }
+    const { id } = await dishRepository.create({ ...dishData, imageId, menuId });
     return await dishRepository.getById(id);
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, 'Dish service create()');
@@ -55,12 +51,17 @@ export const getDishes = async filter => {
     // Count dishes by menu id
     const dishCount = await dishRepository.countAll(filter);
 
-    return {
-      menus: await dishRepository.getAll({
-        ...filter,
-        offset: offset(page, limit) }),
-      totalPage: countPages(dishCount, limit)
-    };
+    return dishCount
+      ? {
+        menus: await dishRepository.getAll(
+          {
+            ...filter,
+            limit,
+            offset: offset(page, limit) }
+        ),
+        totalPage: countPages(dishCount, limit, page)
+      }
+      : [];
   } catch (e) {
     throw new ErrorHandler(e.status, e.message, 'Dish service geAll()');
   }
@@ -68,16 +69,12 @@ export const getDishes = async filter => {
 
 export const update = async (id, data) => {
   try {
-    const { file, user, ...dishBody } = data;
+    const { file, ...dishBody } = data;
+    // TODO те саме що і вресторані і в меню. Лишній раз роблю запит на страву по ID
+    // Перший раз це в міддлеварі, щоб перевірити чи той хто оновлює дані а власником або адміном
+    // А тут щоб дістати imageId, для оновлення картинки і видалення попередньої
     const dish = await getById(id);
 
-    if (dish.menu.restaurant.userId !== user.id && user.role.role !== roles.ADMIN) {
-      throw new ErrorHandler(
-        FORBIDDEN,
-        menuErrorMessages.OWNER_OR_ADMIN_CAN_UPDATE_MENU,
-        'Restaurant update() getById()'
-      );
-    }
     if (file) {
       await imageService.update(dish.image.id, file);
     }

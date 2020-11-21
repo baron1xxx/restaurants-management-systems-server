57e4ba148aef1af2cf99bf89d211f59b82b7ad0d
rommyk <baron1xxx@gmail.com>
2020-11-21 13:59:55 +0200

    Change dish router

--- src/api/routes/dishesRoutes.js
+++ src/api/routes/dishesRoutes.js
@@ -1,33 +1,44 @@
 import { Router } from 'express';
 import jwtAccessTokenMiddleware from '../middlewares/authMiddlewares/jwtAccessTokenMiddleware';
+import roleAuthorizationMiddleware from '../middlewares/authMiddlewares/roleAuthorizationMiddleware';
+import onlyRestaurantOwnerOrAdminMiddleware from '../middlewares/onlyRestaurantOwnerOrAdminMiddleware';
 import createDishValidMiddleware from '../middlewares/dishMiddlewares/createDishValidMiddleware';
 import updateDishValidMiddleware from '../middlewares/dishMiddlewares/updateDishValidMiddleware';
 import dishExistsByNameByMenuIdMiddleware from '../middlewares/dishMiddlewares/dishExistsByNameByMenuIdMiddleware';
 import imageMiddleware from '../middlewares/imageMiddlewares/imageMiddleware';
 import paginationValidateMiddleware from '../middlewares/paginationValidateMiddleware';
 import * as dishController from '../controllers/dishController';
+import { roles } from '../../constants/roles';
 
 const router = Router();
 
 router
-  .get('/:id', dishController.getById)
   .get('/byMenu/:menuId',
     paginationValidateMiddleware,
     dishController.getDishes)
   .post('/',
     jwtAccessTokenMiddleware,
+    roleAuthorizationMiddleware([roles.OWNER, roles.ADMIN]),
     imageMiddleware,
     createDishValidMiddleware,
+    onlyRestaurantOwnerOrAdminMiddleware,
     dishExistsByNameByMenuIdMiddleware,
     dishController.create)
-  .put('/:id',
+  .route('/:dishId')
+  .get(dishController.getById)
+  .all(
     jwtAccessTokenMiddleware,
+    roleAuthorizationMiddleware([roles.OWNER, roles.ADMIN]),
+    onlyRestaurantOwnerOrAdminMiddleware
+  )
+  .put(
     imageMiddleware,
     updateDishValidMiddleware,
     dishExistsByNameByMenuIdMiddleware,
-    dishController.updateDish)
-  .delete('/:id',
-    jwtAccessTokenMiddleware,
-    dishController.removeDish);
+    dishController.updateDish
+  )
+  .delete(
+    dishController.removeDish
+  );
 
 export default router;

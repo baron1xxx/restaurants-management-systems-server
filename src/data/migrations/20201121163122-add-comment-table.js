fd999a240a3d549ec2901fa99d2f756c5700dffc
rommyk <baron1xxx@gmail.com>
2020-11-22 22:22:56 +0200

    Add comment table migration

---
+++ src/data/migrations/20201121163122-add-comment-table.js
@@ -0,0 +1,24 @@
+export default {
+  up: (queryInterface, Sequelize) => queryInterface.sequelize
+    .transaction(transaction => queryInterface.createTable('comments', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true
+      },
+      body: {
+        type: Sequelize.STRING,
+        allowNull: true
+      },
+      isDeleted: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+      },
+      createdAt: Sequelize.DATE,
+      updatedAt: Sequelize.DATE
+    }, { transaction })),
+  down: queryInterface => queryInterface.sequelize
+    .transaction(transaction => queryInterface.dropTable('comments', { transaction }))
+};
